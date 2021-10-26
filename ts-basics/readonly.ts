b419a6f8f1df0993dbccb0d80780c363ccb7b83b
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-10-27 00:59:39 +0500

    completed tutorial 14

---
+++ ts-basics/readonly.ts
@@ -0,0 +1,41 @@
+interface Cat {
+  name: string;
+  breed: string;
+}
+
+function makeCat(name: string, breed: string): Readonly<Cat> {
+  return {
+    name,
+    breed,
+  };
+}
+
+const usul = makeCat("Usul", "Tabby");
+// usul.name = "Piter"; we cant assign as the return type is readonly so it can only be read
+
+class Person {
+  constructor(public readonly name: string, public readonly age: number) {}
+}
+
+const person1 = new Person("Hifz ur Rehman", 24);
+// lgg.name = "Foo"; willl not work as the readonly
+console.log(person1.name);
+
+class StudentList {
+  private persons: Person[] = [];
+
+  static instance: StudentList = new StudentList();
+
+  private constructor() {}
+  // setter
+  static addPerson(person: Person) {
+    StudentList.instance.persons.push(person);
+  }
+  //getter
+  getAllPersons() {
+    return this.persons;
+  }
+}
+
+StudentList.addPerson(person1);
+console.log(StudentList.instance.getAllPersons());
