65632dec541d3f6a878319ef0ee65054cec72dca
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-10-27 00:47:50 +0500

    completed tutorial 13

---
+++ ts-basics/mapped.ts
@@ -0,0 +1,12 @@
+type OnlyBoolsAndHorses = {
+  [key: string]: boolean | string;
+};
+
+const conforms: OnlyBoolsAndHorses = {
+  del: true,
+  rodney: false,
+};
+
+type OptionsFlags<Type> = {
+  [Property in keyof Type]: boolean;
+};
