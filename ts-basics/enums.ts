66ceed2614e70fb16cba2120e918e744ba016e2f
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-10-27 00:10:02 +0500

    completed tutorial 11

---
+++ ts-basics/enums.ts
@@ -0,0 +1,26 @@
+// const beforeLoad = "beforeLoad";
+// const loading = "loading";
+// const loaded = "loaded";
+
+enum LoadingState {
+  beforeLoad = "beforeLoad",
+  loading = "loading",
+  loaded = "loaded",
+}
+
+const englishLoadingStates = {
+  [LoadingState.beforeLoad]: "Before Load",
+};
+
+const isLoading = (state: LoadingState) => state === LoadingState.beforeLoad;
+
+console.log(isLoading(LoadingState.beforeLoad));
+
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0;
+  for (let i = 0; i < dice; i++) {
+    pip += Math.floor(Math.random() * 5) + 1;
+  }
+  return pip;
+}
+console.log(rollDice(3));
